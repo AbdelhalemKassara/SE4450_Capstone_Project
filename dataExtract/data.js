1cb13dfefea00998f404c5e7be7e21ad15c550d9
LiQian Zhou <90808836+edwin20151@users.noreply.github.com>
2023-11-18 17:00:42 -0500

    created json file for all data

--- dataExtract/data.js
+++ dataExtract/data.js
@@ -1,13 +1,24 @@
 const data = require('csvtojson')
-
-const fs = require('fs');
+var JSONStream = require( "JSONStream" );
+const fileSystem = require('fs');
 
 data().fromFile("../Data/csvFiles/2019/2019 Canadian Election Study - Online Survey v1.0.csv").then(data => {
-    console.log(data);
+    var transformStream = JSONStream.stringify();
+    var outputStream = fileSystem.createWriteStream( "./output.json" );
+    transformStream.pipe( outputStream );    
+    data.forEach( transformStream.write );
+    transformStream.end();
+
+outputStream.on(
+    "finish",
+    function handleFinish() {
+        console.log("Done");
+    }
+);
 })
 
 
-data().fromFile("../Data/csvFiles/2019/CES19_dictionarycoding_public_release_final.csv").then(data => {
+/*data().fromFile("../Data/csvFiles/2019/CES19_dictionarycoding_public_release_final.csv").then(data => {
     console.log(data);
 })
 
@@ -26,4 +37,4 @@ data().fromFile("../Data/csvFiles/2021/CES21_dictionarycoding_public_release_fin
 
 data().fromFile("../Data/csvFiles/2022/Democracy Checkup 22_V1.csv").then(data => {
     console.log(data);
-})
+})*/
