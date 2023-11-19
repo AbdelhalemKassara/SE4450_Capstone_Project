7d2f93bc3a43da791f805648499b9af06c1bfe83
LiQian Zhou <90808836+edwin20151@users.noreply.github.com>
2023-11-18 19:54:58 -0500

    data added

--- dataExtract/data.js
+++ dataExtract/data.js
@@ -2,9 +2,9 @@ const data = require('csvtojson')
 var JSONStream = require( "JSONStream" );
 const fileSystem = require('fs');
 
-data().fromFile("../Data/csvFiles/2019/2019 Canadian Election Study - Online Survey v1.0.csv").then(data => {
+ data().fromFile("../Data/csvFiles/2019/2019 Canadian Election Study - Online Survey v1.0.csv").then(data => {
     var transformStream = JSONStream.stringify();
-    var outputStream = fileSystem.createWriteStream( "./output.json" );
+    var outputStream = fileSystem.createWriteStream( "./2019election.json" );
     transformStream.pipe( outputStream );    
     data.forEach( transformStream.write );
     transformStream.end();
@@ -18,23 +18,78 @@ outputStream.on(
 })
 
 
-/*data().fromFile("../Data/csvFiles/2019/CES19_dictionarycoding_public_release_final.csv").then(data => {
-    console.log(data);
+data().fromFile("../Data/csvFiles/2019/CES19_dictionarycoding_public_release_final.csv").then(data => {
+    var transformStream = JSONStream.stringify();
+    var outputStream = fileSystem.createWriteStream( "./2019dictionarycoding.json" );
+    transformStream.pipe( outputStream );    
+    data.forEach( transformStream.write );
+    transformStream.end();
+
+outputStream.on(
+    "finish",
+    function handleFinish() {
+        console.log("Done");
+    }
+);
 })
 
 data().fromFile("../Data/csvFiles/2020/Democracy Checkup 2020 v1.0.csv").then(data => {
-    console.log(data);
-})
+    var transformStream = JSONStream.stringify();
+    var outputStream = fileSystem.createWriteStream( "./2020checkup.json" );
+    transformStream.pipe( outputStream );    
+    data.forEach( transformStream.write );
+    transformStream.end();
+
+outputStream.on(
+    "finish",
+    function handleFinish() {
+        console.log("Done");
+    }
+);
+}) 
 
 
 data().fromFile("../Data/csvFiles/2021/2021 Canadian Election Study v1.0.csv").then(data => {
-    console.log(data);
+    var transformStream = JSONStream.stringify();
+    var outputStream = fileSystem.createWriteStream( "./2021election.json" );
+    transformStream.pipe( outputStream );    
+    data.forEach( transformStream.write );
+    transformStream.end();
+
+outputStream.on(
+    "finish",
+    function handleFinish() {
+        console.log("Done");
+    }
+);
 })
 
 data().fromFile("../Data/csvFiles/2021/CES21_dictionarycoding_public_release_final.csv").then(data => {
-    console.log(data);
+    var transformStream = JSONStream.stringify();
+    var outputStream = fileSystem.createWriteStream( "./2021dictionarycoding.json" );
+    transformStream.pipe( outputStream );    
+    data.forEach( transformStream.write );
+    transformStream.end();
+
+outputStream.on(
+    "finish",
+    function handleFinish() {
+        console.log("Done");
+    }
+);
 })
 
-data().fromFile("../Data/csvFiles/2022/Democracy Checkup 22_V1.csv").then(data => {
-    console.log(data);
-})*/
+ data().fromFile("../Data/csvFiles/2022/Democracy Checkup 22_V1.csv").then(data => {
+    var transformStream = JSONStream.stringify();
+    var outputStream = fileSystem.createWriteStream( "./2022checkup.json" );
+    transformStream.pipe( outputStream );    
+    data.forEach( transformStream.write );
+    transformStream.end();
+
+outputStream.on(
+    "finish",
+    function handleFinish() {
+        console.log("Done");
+    }
+);
+})
