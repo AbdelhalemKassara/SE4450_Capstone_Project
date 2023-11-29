f77ca10fc34cead61aa462e1d504ab7aa23c88b4
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2023-11-29 00:55:03 -0500

    basic setup of vite + react project

---
+++ frontend/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LandingPage from './pages/LandingPage/landingPage.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <LandingPage />
+  </React.StrictMode>,
+)
